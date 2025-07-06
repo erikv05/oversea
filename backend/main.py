e0eee5461ad380773f49bc6a9d1e7a4b0be1bcc2
Victor <erik_vank@brown.edu>
2025-07-06 15:35:04 -0700

    rename

--- backend/main.py
+++ backend/main.py
@@ -744,4 +744,4 @@ async def shutdown_event():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main_fast_debug:app", host="0.0.0.0", port=8000, reload=True, log_level="warning")
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True, log_level="warning")
